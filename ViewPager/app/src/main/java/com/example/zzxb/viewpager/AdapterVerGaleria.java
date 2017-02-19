a47170cb644963c1609e85d5402e3694325ad4e5
zzxb(newmac) <cnhidba@126.com>
2017-02-19 15:39:02 +0800

    新增ViewPager案例

---
+++ ViewPager/app/src/main/java/com/example/zzxb/viewpager/AdapterVerGaleria.java
@@ -0,0 +1,21 @@
+package com.example.zzxb.viewpager;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+
+/**
+ * Created by zzxb on 2017/2/19.
+ */
+
+public class AdapterVerGaleria extends PagerAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return false;
+    }
+}
